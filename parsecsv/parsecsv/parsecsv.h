9c7c6c10426b6ee6323f2077800b14921a86342e
Praveen S <mailpraveens@gmail.com>
2014-08-19 17:28:10 +0530

    Initial commit
    Parsing the file in file path to return the cdv values.

---
+++ parsecsv/parsecsv/parsecsv.h
@@ -0,0 +1,13 @@
+//
+//  parsecsv.h
+//  parsecsv
+//
+//  Created by Praveen S on 8/19/14.
+//  Copyright (c) 2014 mailpraveens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface parsecsv : NSObject
+- (NSArray *) parseCsvIntoArrayFromFilePath:(NSString *) fullFilePath;
+@end
---
+++ parsecsv/parsecsv/parsecsv.m
@@ -0,0 +1,34 @@
+//
+//  parsecsv.m
+//  parsecsv
+//
+//  Created by Praveen S on 8/19/14.
+//  Copyright (c) 2014 mailpraveens. All rights reserved.
+//
+
+#import "parsecsv.h"
+
+@implementation parsecsv
+
+
+- (NSArray *) parseCsvIntoArrayFromFilePath:(NSString *) fullFilePath {
+    NSMutableArray * returnArray = [NSMutableArray  array];
+    NSError * error;
+    NSString* fileContents = [NSString stringWithContentsOfFile:fullFilePath encoding:NSUTF8StringEncoding error:&error];
+    // Strip double quotes
+    NSString * stripDoubleQuotes = [fileContents stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+    // Strip carriage return \r
+    NSString * stripCarriageReturn = [stripDoubleQuotes stringByReplacingOccurrencesOfString:@"\r" withString:@""];
+    NSArray* rows = [stripCarriageReturn componentsSeparatedByString:@"\n"];
+    for (int i = 0; i < [rows count]; i++) {
+        // Skip the first row
+        if (i == 0) {
+            continue;
+        }
+        NSString * row = [rows objectAtIndex:i];
+        [returnArray addObject:[row componentsSeparatedByString:@","]];
+    }
+    return returnArray;
+}
+
+@end
